28636490a43ba6a5615c378ecaed6e399cb94a9e
SankhadeepPramanik <pramaniksankhadeep@gmail.com>
2026-03-17 17:30:59 +0530

    first time

---
+++ string_practice/roman_to_int.py
@@ -0,0 +1,17 @@
+class Solution:
+    def romanToDecimal(self, s): 
+        # code here
+        dic= {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        total=0
+        for i in range(len(s)):
+            if (i<len(s)-1) and (dic[s[i]]<dic[s[i+1]]):
+                total=total-dic[s[i]]
+            else:
+                total=total+dic[s[i]]
+        return total
+
+s= Solution()
+print(s.romanToDecimal("XV"))
